7d34ef1187274bbb11909d5a3cb735b67e95fcf5
kaka <vn503024@gmail.com>
2021-04-06 18:06:07 +0800

    chore(iotedge): quickstarts.sh add the section of creating cognitiveservices

--- iotedge/quickstarts.sh
+++ iotedge/quickstarts.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
 
-echo "Create Resource Group"
-az group create --name KakaEdgeResources --location southeastasia
+RESOURCE_GROUP="KakaEdgeResources"
+LOCATION="southeastasia"
+IOTHUB_NAME="kakaiothub"
+DEVICE_NAME="kakaEdgeDevice"
+CUSTOM_VISION_NAME="kakafactoryai"
+CUSTOM_VISION_TYPE="CustomVision.Training"
+CUSTOM_VISION_LOCATION="westus2"
 
-echo "Create IoT Hub"
-az iot hub create --resource-group KakaEdgeResources --name kakaiothub --sku S1 --partition-count 2
+# echo "Create Resource Group"
+az group create --name ${RESOURCE_GROUP} --location ${LOCATION}
 
-echo "Register an IoT Edge device"
-az iot hub device-identity create --device-id kakaEdgeDevice --edge-enabled --hub-name kakaiothub
+# echo "Create IoT Hub"
+az iot hub create --resource-group ${RESOURCE_GROUP} --name ${IOTHUB_NAME} --sku S1 --partition-count 2
+
+# echo "Register an IoT Edge device"
+az iot hub device-identity create --device-id ${DEVICE_NAME} --edge-enabled --hub-name ${IOTHUB_NAME}
 
 echo "Create VM and install IoT Edge runtime"
 az deployment group create \
---resource-group KakaEdgeResources \
---template-uri "https://aka.ms/iotedge-vm-deploy" \
---parameters dnsLabelPrefix='kaka-edge' \
---parameters adminUsername='kaka' \
---parameters deviceConnectionString=$(az iot hub device-identity connection-string show --device-id kakaEdgeDevice --hub-name kakaiothub -o tsv) \
---parameters authenticationType='password' \
---parameters adminPasswordOrKey="Password123@"
+    --resource-group ${RESOURCE_GROUP} \
+    --template-uri "https://aka.ms/iotedge-vm-deploy" \
+    --parameters dnsLabelPrefix='kaka-edge' \
+    --parameters adminUsername='kaka' \
+    --parameters deviceConnectionString=$(az iot hub device-identity connection-string show --device-id ${DEVICE_NAME} --hub-name ${IOTHUB_NAME} -o tsv) \
+    --parameters authenticationType='password' \
+    --parameters adminPasswordOrKey="Password123@"
+
+echo "Registry a Custom Vision service"
+az cognitiveservices account create \
+    --name ${CUSTOM_VISION_NAME} \
+    --resource-group ${RESOURCE_GROUP} \
+    --kind ${CUSTOM_VISION_TYPE} \
+    --sku S0 \
+    --location ${CUSTOM_VISION_LOCATION} \
+    --yes
